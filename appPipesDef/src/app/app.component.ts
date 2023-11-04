5c5ac6e4afd0a57b54d6d4fab7a9575ec6430ed2
LuisMeraz01227 <luis_mbs05@hotmail.com>
2023-11-03 20:50:10 -0700

    Funciones y estructura appcomponent

--- appPipesDef/src/app/app.component.ts
+++ appPipesDef/src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'appPipesDef';
+
+  nombre = 'Juan Carlos';
+  saldo = 1000.50;
+  dias = ['domingo','lunes','martes','miércoles','jueves','viernes','sábado'];
+  articulos = [{
+      codigo: 1,
+      descripcion: "papas",
+      precio: 12.33
+    },{
+      codigo: 2,
+      descripcion: "manzanas",
+      precio: 54
+    }];
+  fechaActual = new Date();
 }
